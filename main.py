b507ee8e2150e8dd6947979e029bad2a6c5ca098
krishna0136221m <rajaramyadev2@gmail.com>
2023-05-06 23:58:47 +0530

    whether condn

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#this is for check
+print("enter whether condition", end=" ")
+a = input()
+if a == "rainy" or a == "stormy":
+    print("dont go outside ")
+elif a=="cloudy":
+    print("go outside but be carefull")
+elif a == "sunny":
+    print("carry water bottel")
+else:
+    print("go and enjoy")
+print("code executed")
+
+
