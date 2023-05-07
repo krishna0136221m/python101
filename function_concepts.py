3f0d713e43b458a1cee5ac7331aafda9e4725dc1
krishna0136221m <rajaramyadev2@gmail.com>
2023-05-08 00:56:36 +0530

    function concepts

---
+++ function_concepts.py
@@ -0,0 +1,10 @@
+#first  we imported maths_function.py file to use function which is written in that file
+from maths_function import add, subtract, multiply, power, divide, number
+
+print(add(2, 3))
+print(subtract(2, 1))
+print(divide(6, 2))
+print(multiply(2, 2))
+print(power(2, 3))
+
+print(number)
---
+++ maths_function.py
@@ -0,0 +1,24 @@
+number = 20
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def power(a, b):
+    ans = 1
+    for i in range(b):
+        ans *= a
+    return ans
