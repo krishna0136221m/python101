e4067ca4da28c55677c16b86434bc98ccba39327
krishna0136221m <rajaramyadev2@gmail.com>
2023-05-08 22:12:31 +0530

    library concepts

---
+++ list_concepts.py
@@ -0,0 +1,85 @@
+marks = [100, 200, 303, 101, 49, 899]
+# acessing by index
+"""
+print(marks[0])
+print(marks[1])
+print(marks[2])
+"""
+
+"""
+this is method to find length of array
+print(len(marks))
+"""
+
+# sum of number of lists
+"""
+total=0
+for i in range (0,len(marks)):
+    total+=marks[i]
+print(total)
+"""
+
+# another method to add
+# this is also called as for in loop
+"""
+total=0
+for i in marks:
+    print(i, end=" ")
+    total+=i
+print(" ")
+print(total)
+"""
+
+#
+# arr=[]
+# arr.append(60)
+# arr.append(90)
+# arr.append(40)
+# print(arr)
+# arr.sort()
+# print(arr)
+# arr.pop()
+# print(arr)
+# # arr.clear()
+# # print(arr)
+# arr.insert(0,910)
+# print(arr)
+
+# creation of list with some size
+# arr=[]
+# for i in range(20):
+#     arr.append(0)
+# print(len(arr))
+# print(arr)
+
+# 2nd method to create list of certain size
+# arr=[0]*20
+# print(len(arr))
+
+# dynamic list with certain length
+# print("enter length of array")
+# length=int(input())
+# arr=[0]*length
+# print("length of array is ",len(arr))
+
+# arr=[[1,2,3],[4,5,5]]
+# print(arr)
+
+# arr=[[0]*3]*3
+# print(arr)
+
+
+# tupple
+# tup=("krishna",26,26)
+# print(tup)
+# print(tup[0])
+# print(tup[1])
+# print(tup.index("krishna"))
+# print(len(tup))
+# print(tup.count(26))
+
+# list of record of student with their marks
+
+# student = [("krishna", 98), ("rohan", 89), ("abhinav", 79)]
+# for i in student:
+#     print(i, end="\n")
