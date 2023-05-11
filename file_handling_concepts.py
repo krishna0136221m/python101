db90927570f066479b49a88676275b52547bc66a
krishna0136221m <rajaramyadev2@gmail.com>
2023-05-11 23:04:47 +0530

    file handling learning

---
+++ file_handling_concepts.py
@@ -0,0 +1,14 @@
+# file = open("sample.txt","r")
+# # line=file.read()
+# # print(line)
+#
+# print(file.readline())
+# file.close()
+
+# file = open("sample.txt", "w")
+# file.writelines("hello \n this is my file ")
+# file.close()
+
+file = open("sample.txt", "a")
+file.write("this line is appended")
+
