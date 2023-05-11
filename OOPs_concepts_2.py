1b95cf9f96b5f23c45bb964e55a0842d7e193731
krishna0136221m <rajaramyadev2@gmail.com>
2023-05-12 00:15:14 +0530

    oops concept2

---
+++ OOPs_concepts_2.py
@@ -0,0 +1,25 @@
+"""
+polymorphism
+>>overloading
+>operator overloading
+>function overloading
+>>overriding
+
+inheritance
+s
+"""
+
+# operator overloading -> when different /same  operators have different implementations depending on their arguments.
+# print(2+3)
+# print("cse"+ " ccml")
+
+
+# function overloading -> when a function have same name  but different parameters
+# print(len([1,3,6]))
+# print(len("krishna"))
+# print(len((1,3)))
+# print(len({
+#     "name":"krishna",
+#     "profession":"learning"
+# }))
+
